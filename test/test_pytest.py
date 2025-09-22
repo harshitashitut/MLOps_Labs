1d74d4cfdb1fc67bee282263d66b8daf7b3477cc
harshitashitut <harshitashitut@gmail.com>
2025-09-22 19:57:31 -0400

    Updated code in virtual env; fixed src import issue.

--- test/test_pytest.py
+++ test/test_pytest.py
@@ -1,4 +1,8 @@
-from src.calculator import fun1, fun2, fun3, fun4
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from calculator import fun1, fun2, fun3, fun4
+
 
 def test_fun1():
     assert fun1(2, 3) == 5
@@ -10,4 +14,4 @@ def test_fun3():
     assert fun3(2, 3) == 6
 
 def test_fun4():
-    assert fun4(2, 3) == 13
+    assert fun4(2, 3) == 10
--- test/test_unittest.py
+++ test/test_unittest.py
@@ -1,5 +1,8 @@
-import unittest
-from src.calculator import fun1, fun2, fun3, fun4
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+from calculator import fun1, fun2, fun3, fun4
+
 
 class TestCalculator(unittest.TestCase):
     def test_fun1(self):
@@ -12,7 +15,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(fun3(2, 3), 6)
 
     def test_fun4(self):
-        self.assertEqual(fun4(2, 3), 13)
+        self.assertEqual(fun4(2, 3), 10)
 
 if __name__ == '__main__':
     unittest.main()
